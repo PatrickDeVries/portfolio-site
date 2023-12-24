3c1cf40623e964b2a20c6a1c2401432209df2433
Patrick DeVries <pdevries@headstorm.com>
2023-12-24 17:55:07 -0500

    Updates

---
+++ assets/HomeRouter-KXnzU9cP.js
@@ -0,0 +1,7 @@
+import{r as t,_ as o,j as e,u as s}from"./index-idC9n4Zg.js";const a=t.lazy(()=>o(()=>import("./HomePageContent-1wrdzG_P.js"),__vite__mapDeps([0,1,2,3]))),r=()=>e.jsx(t.Suspense,{fallback:"Loading...",children:e.jsx(a,{})}),n=[{path:"/",element:e.jsx(r,{})}],u=()=>s(n);export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/HomePageContent-1wrdzG_P.js","assets/index-idC9n4Zg.js","assets/index-mPipIzqI.css","assets/style-zbxKXRP0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
