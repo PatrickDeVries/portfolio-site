e8ef19123b026e1c687822269a0743ca86eed784
Patrick DeVries <pdevries@headstorm.com>
2023-12-23 22:00:06 -0500

    Add colored threads plugin to portfolio

--- src/portfolio/work/Portfolio.tsx
+++ src/portfolio/work/Portfolio.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../common/components/layout'
-import { myWork } from './constants'
+import { MY_WORK } from './constants'
 import GithubStats from './githubStats'
 import { SectionHeader, WorkItems, Wrapper } from './style'
 import WorkCard from './workCard'
@@ -8,11 +8,11 @@ import WorkCard from './workCard'
 const Portfolio: React.FC = () => (
   <Layout>
     <Wrapper>
-      {Object.keys(myWork).map(section => (
+      {Object.keys(MY_WORK).map(section => (
         <section key={`${section}-section`}>
           <SectionHeader>{section}:</SectionHeader>
           <WorkItems key={`${section}-work-items`}>
-            {myWork[section].map(item => (
+            {MY_WORK[section].map(item => (
               <WorkCard key={item.header} item={item} />
             ))}
             {section === 'Github Contributions' && <GithubStats />}
