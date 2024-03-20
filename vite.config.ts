50eccd1e3beaada275075d65dec1f699b627f51b
Patrick DeVries <pcdevri@gmail.com>
2024-03-20 16:03:05 -0400

    Remove commented code

--- vite.config.ts
+++ vite.config.ts
@@ -34,21 +34,3 @@ export default defineConfig({
     }),
   ],
 })
-
-// <title>Patrick DeVries</title>
-// <meta name="description" content="Personal website of Patrick DeVries">
-
-// <!-- Facebook Meta Tags -->
-// <meta property="og:url" content="https://www.patrickdevries.com/">
-// <meta property="og:type" content="website">
-// <meta property="og:title" content="Patrick DeVries">
-// <meta property="og:description" content="Personal website of Patrick DeVries">
-// <meta property="og:image" content="/open-graph-preview.png">
-
-// <!-- Twitter Meta Tags -->
-// <meta name="twitter:card" content="summary_large_image">
-// <meta property="twitter:domain" content="patrickdevries.com">
-// <meta property="twitter:url" content="https://www.patrickdevries.com/">
-// <meta name="twitter:title" content="Patrick DeVries">
-// <meta name="twitter:description" content="Personal website of Patrick DeVries">
-// <meta name="twitter:image" content="/open-graph-preview.png">
