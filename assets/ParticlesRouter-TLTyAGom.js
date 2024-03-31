7dab527a64d223b413ce8089dbf2edf6389a3963
Patrick DeVries <pcdevri@gmail.com>
2024-03-31 14:03:16 -0400

    Updates

---
+++ assets/ParticlesRouter-TLTyAGom.js
@@ -0,0 +1,7 @@
+import{r as e,_ as s,j as t,u as a}from"./index-l7elDN-U.js";const o=e.lazy(()=>s(()=>import("./ParticlesPageContent-MdbIaInJ.js"),__vite__mapDeps([0,1,2,3]))),r=()=>t.jsx(e.Suspense,{fallback:"Loading...",children:t.jsx(o,{})}),n=[{path:"/",element:t.jsx(r,{})}],i=()=>a(n);export{i as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/ParticlesPageContent-MdbIaInJ.js","assets/index-l7elDN-U.js","assets/index-mPipIzqI.css","assets/style-shM7rwj2.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
