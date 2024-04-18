5913784ae102c45809d51b8b128d46f6dc48e128
Patrick DeVries <pcdevri@gmail.com>
2024-04-18 17:50:25 -0400

    Updates

---
+++ assets/BackgroundEditorRouter--VcIlS1I.js
@@ -0,0 +1,7 @@
+import{r as o,_ as e,j as t,u as s}from"./index-tQt35jQ0.js";const a=o.lazy(()=>e(()=>import("./BackgroundEditorPageContent-7qHXqUlN.js"),__vite__mapDeps([0,1,2]))),r=()=>t.jsx(o.Suspense,{fallback:"Loading...",children:t.jsx(a,{})}),n=[{path:"/",element:t.jsx(r,{})}],u=()=>s(n);export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/BackgroundEditorPageContent-7qHXqUlN.js","assets/index-tQt35jQ0.js","assets/index-mPipIzqI.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
