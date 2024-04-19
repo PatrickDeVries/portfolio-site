8405a1c300266c62b6a966876070ab8f91492b84
Patrick DeVries <pcdevri@gmail.com>
2024-04-19 12:06:47 -0400

    Updates

---
+++ assets/HomeRouter-t_C3Z52S.js
@@ -0,0 +1,7 @@
+import{r as t,_ as o,j as e,u as s}from"./index-maD79-Qe.js";const a=t.lazy(()=>o(()=>import("./HomePageContent-IGb9K2Za.js"),__vite__mapDeps([0,1,2]))),r=()=>e.jsx(t.Suspense,{fallback:"Loading...",children:e.jsx(a,{})}),n=[{path:"/",element:e.jsx(r,{})}],u=()=>s(n);export{u as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/HomePageContent-IGb9K2Za.js","assets/index-maD79-Qe.js","assets/index-mPipIzqI.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- assets/PortfolioRouter-WX4JEKK9.js
+++
@@ -1,7 +0,0 @@
-import{r as t,_ as e,j as o,u as s}from"./index-RP35PAz7.js";const a=t.lazy(()=>e(()=>import("./PortfolioPageContent-H6ODQw2o.js"),__vite__mapDeps([0,1,2]))),n=()=>o.jsx(t.Suspense,{fallback:"Loading...",children:o.jsx(a,{})}),r=t.lazy(()=>e(()=>import("./SSEOPageContent-Z-2rqowW.js"),__vite__mapDeps([3,1,2]))),l=()=>o.jsx(t.Suspense,{fallback:"Loading...",children:o.jsx(r,{})}),i=[{path:"/",element:o.jsx(n,{})},{path:"sseo",element:o.jsx(l,{})}],c=()=>s(i);export{c as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/PortfolioPageContent-H6ODQw2o.js","assets/index-RP35PAz7.js","assets/index-mPipIzqI.css","assets/SSEOPageContent-Z-2rqowW.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
