239fbd9fcac6660c557b6e92e956360072f077d9
Patrick DeVries <pcdevri@gmail.com>
2024-04-22 12:06:05 -0400

    Pre-calculate derived state for lava lamp

---
+++ src/common/components/Layout/Background/LavaLamp/settings-store/derived.ts
@@ -0,0 +1,42 @@
+import { derive } from 'valtio/utils'
+import {
+  LAMP_TEMPERATURE,
+  PARTICLE_COLLISION_RADIUS,
+  PARTICLE_CONDUCTION_COEFFICIENT,
+  PARTICLE_CONVECTION_COEFFICIENT,
+  PARTICLE_VISIBLE_RADIUS,
+} from '../constants'
+import { scaleSetting } from '../utils'
+import lavaLampSettings from './store'
+
+export const derivedLavaLampSettings = derive({
+  scaledParticleVisibleRadius: get =>
+    scaleSetting({
+      base: PARTICLE_VISIBLE_RADIUS,
+      scale: get(lavaLampSettings).particleScale,
+    }),
+  scaledParticleCollisionRadius: get =>
+    scaleSetting({
+      base: PARTICLE_COLLISION_RADIUS,
+      scale: get(lavaLampSettings).particleScale,
+    }),
+  scaledParticleCollisionArea: get =>
+    Math.PI *
+    scaleSetting({
+      base: PARTICLE_COLLISION_RADIUS,
+      scale: get(lavaLampSettings).particleScale,
+    }) **
+      2,
+  scaledCoefficientOfConvection: get =>
+    scaleSetting({
+      base: PARTICLE_CONVECTION_COEFFICIENT,
+      scale: get(lavaLampSettings).convectionCoefficientScale,
+    }),
+  scaledCoefficientOfConduction: get =>
+    scaleSetting({
+      base: PARTICLE_CONDUCTION_COEFFICIENT,
+      scale: get(lavaLampSettings).conductionCoefficientScale,
+    }),
+  scaledLampTemperature: get =>
+    scaleSetting({ base: LAMP_TEMPERATURE, scale: get(lavaLampSettings).lampTempScale }),
+})
--- src/common/components/Layout/Background/LavaLamp/settings-store/index.ts
+++ src/common/components/Layout/Background/LavaLamp/settings-store/index.ts
@@ -1,5 +1,6 @@
 import lavaLampSettings from './store'
 
+export * from './derived'
 export * from './initial'
 export * from './types'
 export * from './utils'
