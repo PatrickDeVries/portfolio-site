264953b293d357b5fe7d25e3421ea278d10aaa53
Patrick DeVries <pcdevri@gmail.com>
2024-04-22 11:15:30 -0400

    Updates

--- assets/PortfolioRouter-aTtRoA96.js
+++
@@ -1,7 +0,0 @@
-import{r as t,_ as e,j as o,u as s}from"./index-maD79-Qe.js";const a=t.lazy(()=>e(()=>import("./PortfolioPageContent-w_1JWk2n.js"),__vite__mapDeps([0,1,2]))),n=()=>o.jsx(t.Suspense,{fallback:"Loading...",children:o.jsx(a,{})}),r=t.lazy(()=>e(()=>import("./SSEOPageContent-Z7vWovz_.js"),__vite__mapDeps([3,1,2]))),l=()=>o.jsx(t.Suspense,{fallback:"Loading...",children:o.jsx(r,{})}),i=[{path:"/",element:o.jsx(n,{})},{path:"sseo",element:o.jsx(l,{})}],c=()=>s(i);export{c as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/PortfolioPageContent-w_1JWk2n.js","assets/index-maD79-Qe.js","assets/index-mPipIzqI.css","assets/SSEOPageContent-Z7vWovz_.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
