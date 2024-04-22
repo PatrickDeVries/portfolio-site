9c953c39384a4170b9840ed743bb42b64c00900e
Patrick DeVries <pcdevri@gmail.com>
2024-04-22 15:56:10 -0400

    Upgrade packages

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -3,11 +3,9 @@ module.exports = {
   customSyntax: 'postcss-styled-syntax',
   rules: {
     'selector-type-case': ['lower', { ignoreTypes: ['/^\\$\\w+/'] }],
-    'declaration-colon-newline-after': null,
     'declaration-empty-line-before': null,
     'no-descending-specificity': null,
     'keyframes-name-pattern': null,
-    'declaration-block-semicolon-newline-after': null,
     'no-empty-source': null,
   },
 }
